3bb9c63d1db83a9597717d0aaa72ca7474bc51a3
Maique <janofstargard@gmail.com>
2024-04-04 03:12:45 -0700

    add valgrind compile options

--- hal/include/hal/buzzer.h
+++ hal/include/hal/buzzer.h
@@ -4,15 +4,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <assert.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
-#include <linux/i2c.h>
-#include <linux/i2c-dev.h>
 #include <pthread.h>
-#include <string.h>
 #include "timing.h"
 
 void Buzzer_init(void);
